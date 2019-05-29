8e569059cb48bc7bb5141c59aaa9f2d121a04250
connerj70 <connerj70@gmail.com>
2019-05-29 07:51:50 -0700

    w

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -8,16 +8,44 @@ export default class Board extends Component {
 
         this.state = {
             squareArr: [0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0],
-            piecesArr: [{type: "Rook", location: 8}, {type: "Horse", location: 9}, {type:"Bishop", location: 10}, {type: "King"}, {type: "Queen"}, {type: "Bishop"}, {type: "Horse"}, {type: "Rook"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}, {type: "pawn"}],
+            piecesArr: [{type: "Rook", location: 0}, {type: "Horse", location: 1}, {type:"Bishop", location: 2}, {type: "King", location: 3}, {type: "Queen", location: 4}, {type: "Bishop", location: 5}, {type: "Horse", location: 6}, {type: "Rook", location: 7}, {type: "pawn", location: 24}, {type: "pawn", location: 9}, {type: "pawn", location: 10}, {type: "pawn", location: 11}, {type: "pawn", location: 12}, {type: "pawn", location: 13}, {type: "pawn", location: 14}, {type: "pawn", location: 39}],
             selected: null
         }
     }
 
     handleClick = (key) => {
-        console.log(key)
-        this.setState({
-            selected: key
-        }, ()=> console.log(this.state.selected))
+        for(let i=0; i < this.state.piecesArr.length; i++) {
+            if(this.state.piecesArr[i].location == key) {
+                this.setState({
+                    selected: key
+                })
+                if(this.state.piecesArr[i].type == "Rook") {
+                    this.handleRookMove(key);
+                }
+            }
+        }
+    }
+
+    handleRookMove = (key) => {
+        var possibleYMovesArr = []
+        var currentLocation;
+        for(let i=0; i < this.state.piecesArr.length; i++) {
+            if(this.state.piecesArr[i].location == key) {
+               currentLocation = this.state.piecesArr[i].location;
+            }
+        }
+        if(currentLocation < 56) {
+            for(var i=currentLocation + 8; i <= 56; i+=8) {
+                possibleYMovesArr.push(i);
+            }
+            for(var j=0; j < this.state.piecesArr.length; j++) {
+                if(possibleYMovesArr.includes(this.state.piecesArr[j].location)) {
+                    console.log(j, 'needs to be removed')
+                   possibleYMovesArr = possibleYMovesArr.slice(0, possibleYMovesArr.indexOf(this.state.piecesArr[j].location));
+                }
+            }
+        }
+        console.log("possible rook moves", possibleYMovesArr);
     }
 
 
@@ -26,9 +54,9 @@ export default class Board extends Component {
             <div className="board-container">
                 {this.state.squareArr.map((val, index) => {
                     if(val === 0) {
-                        return <Square selected={this.state.selected == index ? true : false} key1={index} handleClick={this.handleClick} piece={this.state.piecesArr[index] ? this.state.piecesArr[index].type : null} key={index}/>
+                        return <Square selected={this.state.selected == index ? true : false} key1={index} handleClick={this.handleClick} piece={this.state.piecesArr.map((val)=> {return val.location === index ? val.type : null})} key={index}/>
                     } else {
-                        return <Square selected={this.state.selected == index ? true : false} key1={index} handleClick={this.handleClick} piece={this.state.piecesArr[index] ? this.state.piecesArr[index].type : null} key={index} brown={true} />
+                        return <Square selected={this.state.selected == index ? true : false} key1={index} handleClick={this.handleClick} piece={this.state.piecesArr.map((val)=> {return val.location === index ? val.type : null})} key={index} brown={true} />
                     }
                 })}
             </div>
